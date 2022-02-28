36eb2f2ec1d06015b7ea290d9cc9676307630903
Jacob <callaghanjacob@gmail.com>
2022-02-28 13:56:22 +0000

    added has_player_method

--- single_class_lab_start_code/team_class/src/team.py
+++ single_class_lab_start_code/team_class/src/team.py
@@ -1,9 +1,21 @@
+from pickle import FALSE
+
+
 class Team:
     def __init__(self, name, players, coach):
         self.name = name
         self.players = players
         self.coach = coach
+        self.points = 0
 
     def add_player(self, new_player):
         self.players.append(new_player)   
-        
+
+    def has_player(self, search_name):
+        found = False
+        for player in self.players:
+            if player == search_name:
+                found = True
+                return found
+
+        return found
--- single_class_lab_start_code/team_class/tests/team_test.py
+++ single_class_lab_start_code/team_class/tests/team_test.py
@@ -29,11 +29,11 @@ class TestTeam(unittest.TestCase):
         self.team.add_player(new_player)
         self.assertEqual(5, len(self.team.players))
 
-    @unittest.skip("delete this line to run the test")
+    # @unittest.skip("delete this line to run the test")
     def test_check_player_in_team__found(self):
         self.assertEqual(True, self.team.has_player("Junior Bevil"))
 
-    @unittest.skip("delete this line to run the test")
+    # @unittest.skip("delete this line to run the test")
     def test_check_player_in_team__not_found(self):
         self.assertEqual(False, self.team.has_player("Usain Bolt"))
 
