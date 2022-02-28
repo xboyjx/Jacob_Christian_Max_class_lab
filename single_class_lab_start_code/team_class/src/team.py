8f3fc3aef7e38d7fca6e3bc079d0352f99b3e82f
Jacob <callaghanjacob@gmail.com>
2022-02-28 14:01:27 +0000

    Part B complete

--- single_class_lab_start_code/team_class/src/team.py
+++ single_class_lab_start_code/team_class/src/team.py
@@ -7,6 +7,10 @@ class Team:
         self.players = players
         self.coach = coach
         self.points = 0
+        self.win_points = {
+            True : 3,
+            False : 0
+        }
 
     def add_player(self, new_player):
         self.players.append(new_player)   
@@ -19,3 +23,6 @@ class Team:
                 return found
 
         return found
+
+    def play_game(self, win_status):
+        self.points += self.win_points[win_status]
--- single_class_lab_start_code/team_class/tests/team_test.py
+++ single_class_lab_start_code/team_class/tests/team_test.py
@@ -37,16 +37,16 @@ class TestTeam(unittest.TestCase):
     def test_check_player_in_team__not_found(self):
         self.assertEqual(False, self.team.has_player("Usain Bolt"))
 
-    @unittest.skip("delete this line to run the test")
+    # @unittest.skip("delete this line to run the test")
     def test_team_has_points(self):
         self.assertEqual(0, self.team.points)
         
-    @unittest.skip("delete this line to run the test")
+    # @unittest.skip("delete this line to run the test")
     def test_play_game__win(self):
         self.team.play_game(True)
         self.assertEqual(3, self.team.points)
 
-    @unittest.skip("delete this line to run the test")
+    # @unittest.skip("delete this line to run the test")
     def test_play_game__lose(self):
         self.team.play_game(False)
         self.assertEqual(0, self.team.points)
